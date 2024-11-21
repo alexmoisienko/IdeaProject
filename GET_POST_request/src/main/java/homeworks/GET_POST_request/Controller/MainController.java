896e82ed1ccfa1112f5ddb1586db2290e3bc42e8
Alex <AlexMoisienko@gmail.com>
2024-11-21 17:16:17 +0300

    add map-validation in MainController.java

--- GET_POST_request/src/main/java/homeworks/GET_POST_request/Controller/MainController.java
+++ GET_POST_request/src/main/java/homeworks/GET_POST_request/Controller/MainController.java
@@ -2,9 +2,14 @@ package homeworks.GET_POST_request.Controller;
 
 import homeworks.GET_POST_request.User;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 public class MainController {
@@ -23,6 +28,21 @@ public class MainController {
         User user1 = new User(user.getLogin(), user.getPassword());
 
         return new ResponseEntity<>(user1, HttpStatus.CREATED);
+
+    }
+
+    @PostMapping("/api/main/map")
+    public ResponseEntity<?> POST_main_map(@RequestBody Map<String, @NotEmpty String> map) {
+
+        Object login = map.get("login");
+        Object password = map.get("password");
+
+        if (login != null && password != null) {
+            User user = new User(login.toString(), password.toString());
+            return new ResponseEntity<>(user, HttpStatus.CREATED);
+        } else {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
 
     }
 }

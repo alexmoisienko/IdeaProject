b9cb301525e9250ec94e49a62c9a0b9c65a403a3
Alex <AlexMoisienko@gmail.com>
2024-11-24 22:24:08 +0300

    apdate MainController.java & add jolokia agent

--- GET_POST_request/src/main/java/homeworks/GET_POST_request/Controller/MainController.java
+++ GET_POST_request/src/main/java/homeworks/GET_POST_request/Controller/MainController.java
@@ -3,10 +3,8 @@ package homeworks.GET_POST_request.Controller;
 import homeworks.GET_POST_request.User;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
@@ -34,11 +32,11 @@ public class MainController {
     @PostMapping("/api/main/map")
     public ResponseEntity<?> POST_main_map(@RequestBody Map<String, @NotEmpty String> map) {
 
-        Object login = map.get("login");
-        Object password = map.get("password");
+        String login = map.get("login");
+        String password = map.get("password");
 
         if (login != null && password != null) {
-            User user = new User(login.toString(), password.toString());
+            User user = new User(login, password);
             return new ResponseEntity<>(user, HttpStatus.CREATED);
         } else {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
@@ -46,6 +44,3 @@ public class MainController {
 
     }
 }
-// аннотации стринга валид валидэйт
-//
-// 2 postmapping
